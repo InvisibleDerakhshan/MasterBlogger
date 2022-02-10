c2707a70ea7c8eb1599053b3b1f7007e25370f23
InvisibleDerakhshan <87549134+InvisibleDerakhshan@users.noreply.github.com>
2022-02-10 14:02:21 +0330

    Init repository- article category management implemented

---
+++ MB.Domain/ArticleCategoryAgg/ArticleCategory.cs
@@ -0,0 +1,43 @@
+﻿using MB.Domain.ArticleCategoryAgg.Services;
+using System;
+
+namespace MB.Domain.ArticleCategoryAgg
+{
+    public class ArticleCategory
+    {
+        public ArticleCategory(string title ,IArticleCategoryValidatorService validatorService)
+        {
+          GuardAgianstEmptyTitle(title);
+            validatorService.CheckThatThisRecordAlreadyExists(title);
+            Title = title;
+            IsDeleted = false;
+            CreationDate = DateTime.Now;
+        }
+
+        public long Id { get; private set; }
+        public string Title { get; private set; }
+        public bool IsDeleted { get; private set; }
+        public DateTime CreationDate { get; private set; }
+
+        public void Rename(string title)
+        {
+            GuardAgianstEmptyTitle(title);
+            Title =title;
+        }
+
+        public void Remove()
+        {
+            IsDeleted = true;
+        }
+
+        public void Activate()
+        {
+            IsDeleted=false;
+        }
+        public void GuardAgianstEmptyTitle(string title)
+        {
+            if(string.IsNullOrWhiteSpace(title))
+                throw new ArgumentNullException();
+        }
+    }
+}
